737d67c243320af16f5d7857be3d7668d3ea0578
MaksAnikeev <anikeev.maks@rambler.ru>
2023-01-18 10:36:15 +0300

    Set up admin

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'flower_shop',
-    # 'phonenumber_field',
+    'phonenumber_field'
 ]
 
 MIDDLEWARE = [
@@ -101,7 +101,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
-    os.path.join(BASE_DIR, 'media/')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
