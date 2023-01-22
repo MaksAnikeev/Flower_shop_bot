5c5eb69ff2a3c905804dfa4e15d79a5d1a1f968f
MaksAnikeev <anikeev.maks@rambler.ru>
2023-01-22 19:22:23 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -98,10 +98,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-# STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static/'),
-# ]
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
